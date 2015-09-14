c5aa7d8d77090e525266e6e45355e90506754cef
Karl Sakas <karldonus@gmail.com>
2015-09-14 15:19:59 -0400

    basic CRD, no functional U. will branch to try mongoose instead of mongojs

--- app.js
+++ app.js
@@ -1,36 +1,73 @@
 var express = require('express');
 var app = express();
-var path = require('path');
 var bodyParser = require('body-parser');
-
+var methodOverride = require('method-override');
+var path = require('path');
+var mongojs = require('mongojs');
+var db = mongojs('prospectlist', ['prospectlist']);
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 app.get('/prospectlist', function (req, res){
   console.log("GET request received");
 
-  prospect1 = {
-    firstname: 'Tony',
-    lastname: 'Blair',
-    mobile:'44 20 7925 0918',
-    streetaddress:'10 Downing St',
-    city: 'London',
-    postalcode: 'SW1A 2AA',
-    email: 'tonyblair@uk.gov',
-    qualify: 'Hot'
-  };
-  prospect2 = {
-    firstname: 'Sherlock',
-    lastname: 'Holmes',
-    mobile: '55 21 3245 0113',
-    streetaddress: '221b Baker Street',
-    city: 'London',
-    email: 'sherlock@detective.co.uk',
-    qualify: 'Most Likely to Buy'
-  };
-
-  var prospectlist = [prospect1, prospect2];
-  res.json(prospectlist);
+db.prospectlist.find(function (err, docs) {
+  console.log(docs);
+  res.json(docs);
+  });
 });
+
+app.post('/prospectlist', function (req, res){
+  console.log(req.body);
+  db.prospectlist.insert(req.body, function(err, doc){
+    res.json(doc);
+  });
+});
+
+app.delete('/prospectlist/:id', function (req, res){
+  var id = req.params.id;
+  console.log(id);
+  db.prospectlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc){
+    res.json(doc);
+  });
+});
+
+app.get('/prospectlist/:id', function(req, res){
+  var id = req.params.id;
+  console.log(id);
+  db.prospectlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc){
+    res.json(doc);
+  });
+});
+
+app.put('prospectlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(req.body.firstname);
+});
+
+  // prospect1 = {
+  //   firstname: 'Tony',
+  //   lastname: 'Blair',
+  //   mobile:'44 20 7925 0918',
+  //   streetaddress:'10 Downing St',
+  //   city: 'London',
+  //   postalcode: 'SW1A 2AA',
+  //   email: 'tonyblair@uk.gov',
+  //   qualify: 'Hot'
+  // };
+  // prospect2 = {
+  //   firstname: 'Sherlock',
+  //   lastname: 'Holmes',
+  //   mobile: '55 21 3245 0113',
+  //   streetaddress: '221b Baker Street',
+  //   city: 'London',
+  //   email: 'sherlock@detective.co.uk',
+  //   qualify: 'Cold'
+  // };
+  //
+  // var prospectlist = [prospect1, prospect2];
+  // res.json(prospectlist);
+
 
 app.set('port', (process.env.PORT || 4444));
 
--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,9 +2,46 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("controller communicating");
 
-$http.get('/prospectlist').success(function(res) {
-  console.log("data sent from controller");
-  $scope.prospectlist = res;
-});
+var refresh = function(){
+  $http.get('/prospectlist').success(function(res) {
+    console.log("data sent from controller");
+    $scope.prospectlist = res;
+    $scope.prospect = "";
+  });
+};
+
+refresh();
+
+$scope.addProspect = function() {
+  console.log($scope.prospect);
+  $http.post('/prospectlist', $scope.prospect).success(function(response){
+    console.log(response);
+    refresh();
+  });
+};
+
+$scope.remove = function(id) {
+  console.log(id);
+  $http.delete('/prospectlist/' + id).success(function(response){
+    refresh();
+  });
+};
+
+// $scope.update = function() {
+//   console.log($scope.prospect._id);
+// };
+
+// $scope.edit = function(id) {
+//   console.log(id);
+//   $http.get('/prospectlist/' + id).success(function(response){
+//     $scope.prospect = response;
+//   });
+// };
+
+$scope.update = function(id){
+  console.log(id);
+  $http.put('/prospectlist/' + id);
+};
+
 
 }]);
